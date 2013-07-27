81a1f352e20ff2c9893bc854e09e772243dd3c0a
Bowen Masco <bowen@codingfoo.com>
2013-07-27 00:41:16 -0600

    fix maps controller specs

--- spec/controllers/maps_controller_spec.rb
+++ spec/controllers/maps_controller_spec.rb
@@ -23,7 +23,7 @@ describe Api::MapsController do
   # This should return the minimal set of attributes required to create a valid
   # Api::Map. As you add validations to Api::Map, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { {  } }
+  let(:valid_attributes) { { title: 'foo' } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
@@ -32,67 +32,52 @@ describe Api::MapsController do
 
   describe "GET index" do
     it "assigns all api_maps as @api_maps" do
-      map = Api::Map.create! valid_attributes
-      get :index, {}, valid_session
-      assigns(:api_maps).should eq([map])
+      map = Map.create! valid_attributes
+      get :index, {format: :json}, valid_session
+      assigns(:maps).should eq([map])
     end
   end
 
   describe "GET show" do
     it "assigns the requested api_map as @api_map" do
-      map = Api::Map.create! valid_attributes
-      get :show, {:id => map.to_param}, valid_session
-      assigns(:api_map).should eq(map)
-    end
-  end
-
-  describe "GET new" do
-    it "assigns a new api_map as @api_map" do
-      get :new, {}, valid_session
-      assigns(:api_map).should be_a_new(Api::Map)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested api_map as @api_map" do
-      map = Api::Map.create! valid_attributes
-      get :edit, {:id => map.to_param}, valid_session
-      assigns(:api_map).should eq(map)
+      map = Map.create! valid_attributes
+      get :show, {:id => map.to_param, format: :json}, valid_session
+      assigns(:map).should eq(map)
     end
   end
 
   describe "POST create" do
     describe "with valid params" do
-      it "creates a new Api::Map" do
+      it "creates a new Map" do
         expect {
-          post :create, {:api_map => valid_attributes}, valid_session
-        }.to change(Api::Map, :count).by(1)
+          post :create, {:map => valid_attributes, format: :json}, valid_session
+        }.to change(Map, :count).by(1)
       end
 
       it "assigns a newly created api_map as @api_map" do
-        post :create, {:api_map => valid_attributes}, valid_session
-        assigns(:api_map).should be_a(Api::Map)
-        assigns(:api_map).should be_persisted
+        post :create, {:map => valid_attributes, format: :json}, valid_session
+        assigns(:map).should be_a(Map)
+        assigns(:map).should be_persisted
       end
 
       it "redirects to the created api_map" do
-        post :create, {:api_map => valid_attributes}, valid_session
-        response.should redirect_to(Api::Map.last)
+        post :create, {:map => valid_attributes, format: :json}, valid_session
+        response.should redirect_to(Map.last)
       end
     end
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved api_map as @api_map" do
         # Trigger the behavior that occurs when invalid params are submitted
-        Api::Map.any_instance.stub(:save).and_return(false)
-        post :create, {:api_map => {  }}, valid_session
-        assigns(:api_map).should be_a_new(Api::Map)
+        Map.any_instance.stub(:save).and_return(false)
+        post :create, {:map => {  }, format: :json}, valid_session
+        assigns(:map).should be_a_new(Map)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
-        Api::Map.any_instance.stub(:save).and_return(false)
-        post :create, {:api_map => {  }}, valid_session
+        Map.any_instance.stub(:save).and_return(false)
+        post :create, {:map => {  }, format: :json}, valid_session
         response.should render_template("new")
       end
     end
@@ -101,42 +86,42 @@ describe Api::MapsController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested api_map" do
-        map = Api::Map.create! valid_attributes
+        map = Map.create! valid_attributes
         # Assuming there are no other api_maps in the database, this
-        # specifies that the Api::Map created on the previous line
+        # specifies that the Map created on the previous line
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
-        Api::Map.any_instance.should_receive(:update).with({ "these" => "params" })
-        put :update, {:id => map.to_param, :api_map => { "these" => "params" }}, valid_session
+        Map.any_instance.should_receive(:update).with({ "these" => "params" })
+        put :update, {:id => map.to_param, :map => { "these" => "params" }, format: :json}, valid_session
       end
 
       it "assigns the requested api_map as @api_map" do
-        map = Api::Map.create! valid_attributes
-        put :update, {:id => map.to_param, :api_map => valid_attributes}, valid_session
-        assigns(:api_map).should eq(map)
+        map = Map.create! valid_attributes
+        put :update, {:id => map.to_param, :map => valid_attributes, format: :json}, valid_session
+        assigns(:map).should eq(map)
       end
 
       it "redirects to the api_map" do
-        map = Api::Map.create! valid_attributes
-        put :update, {:id => map.to_param, :api_map => valid_attributes}, valid_session
+        map = Map.create! valid_attributes
+        put :update, {:id => map.to_param, :map => valid_attributes, format: :json}, valid_session
         response.should redirect_to(map)
       end
     end
 
     describe "with invalid params" do
       it "assigns the api_map as @api_map" do
-        map = Api::Map.create! valid_attributes
+        map = Map.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
-        Api::Map.any_instance.stub(:save).and_return(false)
-        put :update, {:id => map.to_param, :api_map => {  }}, valid_session
-        assigns(:api_map).should eq(map)
+        Map.any_instance.stub(:save).and_return(false)
+        put :update, {:id => map.to_param, :map => {  }, format: :json}, valid_session
+        assigns(:map).should eq(map)
       end
 
       it "re-renders the 'edit' template" do
-        map = Api::Map.create! valid_attributes
+        map = Map.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
-        Api::Map.any_instance.stub(:save).and_return(false)
-        put :update, {:id => map.to_param, :api_map => {  }}, valid_session
+        Map.any_instance.stub(:save).and_return(false)
+        put :update, {:id => map.to_param, :map => {  }, format: :json}, valid_session
         response.should render_template("edit")
       end
     end
@@ -144,15 +129,15 @@ describe Api::MapsController do
 
   describe "DELETE destroy" do
     it "destroys the requested api_map" do
-      map = Api::Map.create! valid_attributes
+      map = Map.create! valid_attributes
       expect {
-        delete :destroy, {:id => map.to_param}, valid_session
-      }.to change(Api::Map, :count).by(-1)
+        delete :destroy, {:id => map.to_param, format: :json}, valid_session
+      }.to change(Map, :count).by(-1)
     end
 
     it "redirects to the api_maps list" do
-      map = Api::Map.create! valid_attributes
-      delete :destroy, {:id => map.to_param}, valid_session
+      map = Map.create! valid_attributes
+      delete :destroy, {:id => map.to_param, format: :json}, valid_session
       response.should redirect_to(api_maps_url)
     end
   end
