a5979273f0dfde69129449e57c4ce629448cebf6
Bowen Masco <bowen@codingfoo.com>
2013-07-27 00:40:58 -0600

    add map requests specs

--- spec/requests/maps_spec.rb
+++ spec/requests/maps_spec.rb
@@ -1,11 +1,43 @@
 require 'spec_helper'
 
-describe "Api::Maps" do
-  describe "GET /api_maps" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get api_maps_path
-      response.status.should be(200)
+describe "Maps" do
+  describe "GET /maps" do
+    it "retrieves an collection of maps" do
+      Map.create! title: 'foo1'
+      Map.create! title: 'foo1'
+      get api_maps_path format: :json
+      expect(JSON.parse(response.body)['maps'].count).to eq(2)
+    end
+  end
+
+  describe "GET /maps/:id" do
+    it "retrieves a map" do
+      @map = Map.create! title: 'foo'
+      get api_map_path(@map.id), format: :json
+      expect(JSON.parse(response.body)['id']).to eq(@map.id)
+    end
+  end
+
+  describe "POST /maps" do
+    it "creates a map" do
+      post api_maps_path, map: { title: "foo"}, format: :json
+      expect(JSON.parse(response.body)['title']).to eq('foo')
+    end
+  end
+
+  describe "DELETE /maps/:id" do
+    it "deletes a map" do
+      Map.create! title: 'foo'
+      @map = Map.create! title: 'foo2'
+      expect{delete api_map_path(@map.id), format: :json}.to change{Map.all.count}.by(-1)
+    end
+  end
+
+  describe "PUT /maps/:id" do
+    it "updates a map" do
+      map = Map.create! title: 'foobar'
+      put api_map_path(map.id), map: { title: 'bar', cargo: "hold" }, format: :json
+      expect(map.reload.title).to eq('bar')
     end
   end
 end
