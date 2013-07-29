66eb9995a8a8bc494a7e1716f718b3bd35ecd59f
Justin Ball <justinball@gmail.com>
2013-07-29 13:58:45 -0600

    Removed active_model_serializers

--- config/initializers/serializers.rb
+++ config/initializers/serializers.rb
@@ -1,5 +1,6 @@
-ActiveSupport.on_load(:active_model_serializers) do
-  ActiveRecord::Base.include_root_in_json = true
-  ActiveModel::Serializer.root = true
-  ActiveModel::ArraySerializer.root = true
-end
+ActiveRecord::Base.include_root_in_json = true
+
+# ActiveSupport.on_load(:active_model_serializers) do
+#   ActiveModel::Serializer.root = true
+#   ActiveModel::ArraySerializer.root = true
+# end
