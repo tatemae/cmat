57fb6e2be306326a48b93cd0b5a711c7023837b1
Justin Ball <justinball@gmail.com>
2013-07-30 15:13:13 -0600

    Removed Junk comments

--- config/initializers/serializers.rb
+++ config/initializers/serializers.rb
@@ -1,6 +1,1 @@
 ActiveRecord::Base.include_root_in_json = true
-
-# ActiveSupport.on_load(:active_model_serializers) do
-#   ActiveModel::Serializer.root = true
-#   ActiveModel::ArraySerializer.root = true
-# end
