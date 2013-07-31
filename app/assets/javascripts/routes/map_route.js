bed512223db3a2f1586f5e6b9dbb1461dcdc9752
Justin Ball <justinball@gmail.com>
2013-07-31 15:58:59 -0600

    Fix for map route

--- app/assets/javascripts/routes/map_route.js
+++ app/assets/javascripts/routes/map_route.js
@@ -2,12 +2,7 @@ var MapModel = require('../models/map');
 var MapRoute = Ember.Route.extend({
 
   model: function(params){
-    //return MapModel.find(params.map_id);
-    // Temp HACK until we get API in place
-    //App.FlashMessages.pushFlash('notice', 'The flash works!.');
-    return MapModel.createRecord({
-      name: 'Map'
-    });
+    return MapModel.find(params.map_id);
   }
 
 });
