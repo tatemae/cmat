22f48fa46261197e2c848849631de2c0a6be49d5
Bowen Masco <bowen@codingfoo.com>
2013-07-31 16:15:19 -0600

    add description and displayName to Objective Map

--- app/assets/javascripts/models/objective_bank.js
+++ app/assets/javascripts/models/objective_bank.js
@@ -2,15 +2,39 @@ var ModelBase = require('./model_base');
 
 var ObjectiveBank = ModelBase.extend({
   current: DS.attr('boolean'),
-  genusTypeId: DS.attr('string')
+  genusTypeId: DS.attr('string'),
+  description: DS.attr('description'),
+  displayName: DS.attr('displayName')
 });
 
-App.Store.registerAdapter('App.ObjectiveBank', DS.Adapter.extend({
+var adapter = DS.Adapter.extend({
   find: function(store, type, id) {
     $.getJSON('https://oki-dev.mit.edu/handcar/services/learning/objectivebanks/'+id).then( function(json) {
       store.load(type, json);
     });
   }
-}));
+});
+
+adapter.registerTransform('description', {
+  serialize: function(value) {
+    return Em.isNone(value) ? {} : value;
+  },
+
+  deserialize: function(value) {
+    return Em.isNone(value) ? {} : value;
+  }
+});
+
+adapter.registerTransform('displayName', {
+  serialize: function(value) {
+    return Em.isNone(value) ? {} : value;
+  },
+
+  deserialize: function(value) {
+    return Em.isNone(value) ? {} : value;
+  }
+});
+
+App.Store.registerAdapter('App.ObjectiveBank', adapter);
 
 module.exports = ObjectiveBank;
