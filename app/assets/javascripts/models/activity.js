592baf12059be44bc253c6c9b07eac550f6a7cd3
Bowen Masco <bowen@codingfoo.com>
2013-08-05 11:52:22 -0600

    add activity model

---
+++ app/assets/javascripts/models/activity.js
@@ -0,0 +1,32 @@
+var ModelBase = require('./model_base');
+
+var Activity = Ember.Object.extend({
+
+});
+
+Activity.reopenClass({
+  findQuery: function(query){
+    return new Ember.RSVP.Promise(function(resolve, reject){
+
+      var url = 'https://oki-dev.mit.edu/handcar/services/learning/objectivebanks/'+query['objectivebank']
+
+      if( query['objective'] )
+      {
+        url = url + '/objectives/' + query['objective']
+      }
+
+      url = url + '/activities'
+
+      resolve($.getJSON(url).then(function(response){
+        var activities = Em.A();
+        response.forEach(function (activity) {
+          activities.pushObject(App.Activity.create(activity));
+        });
+        return activities;
+      }));
+
+    });
+  }
+});
+
+module.exports = Activity;
