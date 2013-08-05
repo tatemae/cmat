ef08464c744d0eb77300ce51ccb05193297369ed
Justin Ball <justinball@gmail.com>
2013-08-05 12:13:59 -0600

    code refactor

--- app/assets/javascripts/routes/maps_route.js
+++ app/assets/javascripts/routes/maps_route.js
@@ -5,34 +5,36 @@ MapsRoute = Ember.Route.extend({
   afterModel: function(model, transition){
     if(transition.targetName == "maps.index"){
       transition.abort();
-      var _self = this;
-      var current_user = this.controllerFor('currentUser');
-      if(current_user){
-        var user_id = current_user.get('id');
-        if(user_id){
-          return MapModel.find({user_id: user_id}).then(function(maps){
-            var map = maps.objectAt(0);
-            if(map){
-              _self.transitionTo('map', map);
-            } else {
-              console.log('no map found creating map');
-              _self.transitionToNewMap();
-            }
-          });
-        }
-      }
-      this.transitionToNewMap();
+      this.transitionToMap();
     }
   },
 
-  transitionToNewMap: function(){
+  transitionToMap: function(){
     var _self = this;
+    var current_user = this.controllerFor('currentUser');
+    if(!Ember.isEmpty(current_user)){
+      var user_id = current_user.get('id');
+      if(!Ember.isEmpty(user_id)){
+        return MapModel.find({user_id: user_id}).then(function(maps){
+          var map = maps.objectAt(0);
+          if(!Ember.isEmpty(map)){
+            _self.transitionTo('map', map);
+          } else {
+            _self.transitionToNewMap();
+          }
+        });
+      }
+    }
+    this.transitionToNewMap();
+  },
+
+  transitionToNewMap: function(){
     var map = MapModel.createRecord({
       title: 'New Map'
     });
     map.save().then(function(){
-      _self.transitionTo('map', map);
-    });
+      this.transitionTo('map', map);
+    }.bind(this));
   }
 
 });
