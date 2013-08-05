2c4acb2ef6ff34ae50034bbb7a2267d90da02b59
Bowen Masco <bowen@codingfoo.com>
2013-08-05 15:11:43 -0600

    one layer of outcomes per row

--- app/assets/javascripts/models/map.js
+++ app/assets/javascripts/models/map.js
@@ -35,7 +35,7 @@ var Map = ModelBase.extend({
         var workspaceWidth = UI.cmat_app.canvasWidth();
         var workspaceHeight = UI.cmat_app.canvasHeight();
 
-        var x = deltaX / 2;
+        var x = workspaceWidth / 2;
         var y = deltaY;
         var number_of_nodes = objectives.length;
         var nodes_per_row = (workspaceWidth / deltaX) - 1;
@@ -52,7 +52,8 @@ var Map = ModelBase.extend({
           nodes[objectives[objective_index]['id']] = parent;
           App.Objective.findQuery({objectivebank: objectiveBank.get('id'), objective: objectives[objective_index]['id'], children: true, parent: parent}).then(function(data){
             var children = data[0],
-                childs_parent = data[1];
+                childs_parent = data[1],
+                child;
             if(children.length == 0)
             {
               return;
@@ -61,38 +62,53 @@ var Map = ModelBase.extend({
             var child_y = childs_parent.attrs['y']+deltaY;
             var children_width = (children.length-1)*deltaX;
             var child_x = childs_parent.attrs['x']-children_width/2;
+            var cmat_child;
             for( var child_index = 0; child_index < children.length; child_index++ ) {
-              UI.cmat_app.addNode(_self.mc3_to_cmat(children[child_index], child_x, child_y), childs_parent, false);
+              cmat_child = _self.mc3_to_cmat(children[child_index], child_x, child_y);
+              if(nodes[cmat_child['id']] )
+              {
+                UI.cmat_app.addConnection({}, markerRadius, childs_parent, nodes[cmat_child['id']]);
+              }
+              else
+              {
+                child = UI.cmat_app.addNode(cmat_child, childs_parent, false);
+                nodes[child['id']] = child;
+              }
+
               child_x += deltaX;
+
+            // App.Activity.findQuery({objectivebank: objectiveBank.get('id'), objective: childs}).then(function(activities){
+            //   var activity;
+            //   var activity_attrs;
+
+            //   for( var activity_index = 0; activity_index < activities.length; activity_index++ ) {
+            //     activity_attrs = _self.mc3_to_cmat(activities[activity_index], x, y)
+            //     activity_parent = nodes[activities[activity_index]['objectiveId']];
+            //     activity = UI.cmat_app.addNode(activity_attrs, activity_parent, false);
+            //   }
+            // });
+
             }
+
             UI.getStage().draw();
           });
-          x += deltaX;
-          if (x > workspaceWidth) {
-            even_row = !even_row;
-            var remainingNodes = number_of_nodes - objective_index;
-            if (remainingNodes < nodes_per_row) {
-              x = (workspaceWidth - (remainingNodes-2)*deltaX) / 2;
-            } else {
-              x = deltaX / 2;
-            }
-            if (even_row) {
-              x += deltaX / 2;
-            }
-            y += deltaY*2;
-          }
+          // x += deltaX;
+          // if (x > workspaceWidth) {
+            // even_row = !even_row;
+            // var remainingNodes = number_of_nodes - objective_index;
+            // if (remainingNodes < nodes_per_row) {
+            //   x = (workspaceWidth - (remainingNodes-2)*deltaX) / 2;
+            // } else {
+            //   x = deltaX / 2;
+            // }
+            // if (even_row) {
+            //   x += deltaX / 2;
+            // }
+            y += deltaY*3;
+          // }
         }
 
-        // App.Activity.findQuery({objectivebank: objectiveBank.get('id')}).then(function(activities){
-        //   var activity;
-        //   var activity_attrs;
 
-        //   for( var activity_index = 0; activity_index < activities.length; activity_index++ ) {
-        //     activity_attrs = _self.mc3_to_cmat(activities[activity_index], x, y)
-        //     activity_parent = nodes[activities[activity_index]['objectiveId']];
-        //     activity = UI.cmat_app.addNode(activity_attrs, activity_parent, false);
-        //   }
-        // });
 
         console.log(number_of_nodes);
 
