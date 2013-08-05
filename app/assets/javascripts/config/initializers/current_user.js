e20ee0c0b2459024f5a974cf457653ad973eacc7
Justin Ball <justinball@gmail.com>
2013-08-05 11:36:04 -0600

    Only use initializer to load user from meta tag

--- app/assets/javascripts/config/initializers/current_user.js
+++ app/assets/javascripts/config/initializers/current_user.js
@@ -6,15 +6,11 @@ Ember.Application.initializer({
   initialize: function(container) {
     var store = container.lookup('store:main');
     var attributes = $('meta[name="current-user"]').attr('content');
-    var user;
     if(attributes){
       var object = store.load(User, JSON.parse(attributes));
-      user = User.find(object.id);
-    } else {
-      user = User.find('current');
+      var user = User.find(object.id);
+      container.lookup('controller:currentUser').set('content', user);
+      container.typeInjection('controller', 'currentUser', 'controller:currentUser');
     }
-
-    container.lookup('controller:currentUser').set('content', user);
-    container.typeInjection('controller', 'currentUser', 'controller:currentUser');
   }
 });
