48071ae29fb9f71ddf41838f28d0f43ba5bd0593
Bowen Masco <bowen@codingfoo.com>
2013-08-12 09:01:39 -0600

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Cmat::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -52,7 +52,7 @@ Cmat::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   config.logger = Logger.new(STDOUT)
-  config.logger.level = Logger::INFO
+  config.logger.level = Logger::DEBUG
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
