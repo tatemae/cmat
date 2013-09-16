d1a13dd570110e0e05b7e43fc0aece0403c7aef9
Bowen Masco <bowen@codingfoo.com>
2013-09-15 22:15:24 -0600

    add assets methods

--- app/assets/javascripts/models/asset.js
+++ app/assets/javascripts/models/asset.js
@@ -9,5 +9,36 @@ Cmat.Asset.reopenClass({
       var url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+objective_bank_id+'/assets'
 
       return $.getJSON(url);
+  },
+  saveNew: function(activity, parent) {
+    return new Ember.RSVP.Promise(function(resolve, reject){
+      var url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+activity['objectiveBankId']+'/assets';
+      // An activity requires the following fields:
+      // description.text
+      // displayName.text
+      // genusTypeID - must be a valid type
+      // objectiveBankId
+      resolve($.ajax({type: "POST", url: url, data: JSON.stringify(activity), contentType: "application/json", dataType: 'json'}).then(function(response){
+        return response;
+      }));
+    });
+  },
+  saveChanges: function(objective) {
+    return new Ember.RSVP.Promise(function(resolve, reject){
+      var url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+objective['objectiveBankId']+'/assets';
+
+      resolve($.ajax({type: "PUT", url: url, data: JSON.stringify(objective), contentType: "application/json", dataType: 'json'}).then(function(response){
+        return response;
+      }));
+    });
+  },
+  deleteNode: function(query){
+    return new Ember.RSVP.Promise(function(resolve, reject){
+      var url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+query['objectiveBankId']+'/assets/'+query['id'];
+
+      resolve($.ajax({type: "DELETE", url: url, contentType: "application/json", dataType: 'json'}).then(function(response){
+        return response;
+      }));
+    });
   }
 });
