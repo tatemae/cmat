5957c0f75a8bcf8e04482a8738642e7acab57e5a
James Carbine <JamesCarbine@gmail.com>
2013-09-18 17:02:00 -0600

    removed old ListBox component and using Ember.Select for node type. Setting the node types dynamically depending on what is happening.

--- app/assets/javascripts/controllers/node_controller.js
+++ app/assets/javascripts/controllers/node_controller.js
@@ -1,12 +1,26 @@
 Cmat.NodeController = Ember.ObjectController.extend({
 
-  types: ['topic', 'outcome', 'activity', 'asset - url', 'asset - unknown'],
+  types: null,
 
   changeState: function(){
     if(this.get('state') == 'edit'){
+      if (this.get('type') === 'activity'){
+        this.set('types', ['activity']);
+        this.set('isDisabled', true);
+      }else{
+        this.set('types', ['topic', 'outcome']);
+        this.set('isDisabled', false);
+      }
       this.transitionToRoute('node.edit');
     }
     if(this.get('state') == 'new'){
+      this.set('isDisabled', false);
+      this.set('type', 'topic');
+      if (this.get('parent')){
+        this.set('types', ['topic', 'outcome', 'activity']);
+      }else{
+        this.set('types', ['topic', 'outcome']);
+      }
       this.transitionToRoute('node.new');
     }
   }.observes('state'),
