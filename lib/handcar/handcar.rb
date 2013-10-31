8ce7635d6101b6dd986e8e00430af2e0aa21c053
Bowen Masco <bowen@codingfoo.com>
2013-10-31 15:50:42 -0600

    initial handcar request

--- lib/handcar/handcar.rb
+++ lib/handcar/handcar.rb
@@ -1,10 +1,9 @@
 require 'open-uri'
 
-module Handcar
+class Handcar
   URL = 'https://mc3-demo.mit.edu/handcar/services/authentication/agentkeys/'
 
   def self.fetch_new_user_key
-    url = "#{URL}#{ENV['agent_id']}?proxyname=#{ENV['agent_key']}"
-    open(url)
+    open("#{URL}#{ENV['agent_id']}?proxyname=#{ENV['agent_key']}").read
   end
 end
---
+++ spec/lib/handcar/handcar_spec.rb
@@ -0,0 +1,8 @@
+require 'lib/handcar/handcar'
+
+describe Handcar do
+  it "fetches a new user key" do
+    expect( Handcar.fetch_new_user_key[0..8] ).to eq("AGENT_KEY")
+  end
+
+end
