e8a005e79c9e16455c5accf07a520eccbee0f513
Bowen Masco <bowen@codingfoo.com>
2013-11-01 13:57:03 -0600

    add duration to key request

--- lib/handcar/handcar.rb
+++ lib/handcar/handcar.rb
@@ -4,6 +4,6 @@ class Handcar
   URL = 'https://mc3-demo.mit.edu/handcar/services/authentication/agentkeys/'
 
   def self.fetch_user_key
-    open("#{URL}#{ENV['agent_id']}?proxyname=#{ENV['agent_key']}").read
+    open("#{URL}#{ENV['agent_id']}?duration=3600&proxyname=#{ENV['agent_key']}").read
   end
 end
