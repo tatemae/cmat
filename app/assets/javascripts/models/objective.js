a7bec79633d8d5d08734a667d8499b7c5a125c91
Bowen Masco <bowen@codingfoo.com>
2013-11-01 13:57:32 -0600

    add key to requests

--- app/assets/javascripts/models/objective.js
+++ app/assets/javascripts/models/objective.js
@@ -4,28 +4,39 @@ Cmat.Objective = Ember.Object.extend({
 
 Cmat.Objective.reopenClass({
   findQuery: function(query){
-      var objective_bank_id = query['objective_bank_id'];
+    var objective_bank_id = query['objective_bank_id'];
 
-      var url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+objective_bank_id+'/objectives'
+    var url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+objective_bank_id+'/objectives'
 
-      if(query['children'])
-      {
-        url = url + '/' + query['objective'] + '/childids'
-      }
-      else if(query['roots'])
-      {
-        url = url + '/rootids'
-      }
-      else if(query['bulk'])
-      {
-        url = url + '/' + query['objective'] + '/children/bulk'
-      }
+    if(query['children'])
+    {
+      url = url + '/' + query['objective'] + '/childids'
+    }
+    else if(query['roots'])
+    {
+      url = url + '/rootids'
+    }
+    else if(query['bulk'])
+    {
+      url = url + '/' + query['objective'] + '/children/bulk'
+    }
 
-      return $.getJSON(url);
+    if( Config.settings.user_auth_token() )
+    {
+      url = url + '?proxyname=' + Config.settings.user_auth_token();
+    }
+
+    return $.getJSON(url);
   },
   saveNew: function(objective, parent) {
     return new Ember.RSVP.Promise(function(resolve, reject){
       var url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+objective['objectiveBankId']+'/objectives';
+
+      if( Config.settings.user_auth_token() )
+      {
+        url = url + '?proxyname=' + Config.settings.user_auth_token();
+      }
+
       // An objective requires the following fields:
       // description.text
       // displayName.text
@@ -39,12 +50,19 @@ Cmat.Objective.reopenClass({
   saveParentRelationship: function(objectiveBankId, childId, parentIds) {
     var relationship = {ids: parentIds};
     var relationship_url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+objectiveBankId+'/objectives/'+childId+'/parentids';
+    if( Config.settings.user_auth_token() )
+    {
+      relationship_url = relationship_url + '?proxyname=' + Config.settings.user_auth_token();
+    }
     $.ajax({type: "PUT", url: relationship_url, data: JSON.stringify(relationship), contentType: "application/json", dataType: 'json'})
   },
   saveChanges: function(objective) {
     return new Ember.RSVP.Promise(function(resolve, reject){
       var url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+objective['objectiveBankId']+'/objectives';
-
+      if( Config.settings.user_auth_token() )
+      {
+        url = url + '?proxyname=' + Config.settings.user_auth_token();
+      }
       resolve($.ajax({type: "PUT", url: url, data: JSON.stringify(objective), contentType: "application/json", dataType: 'json'}).then(function(response){
         return response;
       }));
@@ -53,7 +71,10 @@ Cmat.Objective.reopenClass({
   deleteNode: function(query){
     return new Ember.RSVP.Promise(function(resolve, reject){
       var url = Config.settings.cmat_base_url + '/services/learning/objectivebanks/'+query['objectiveBankId']+'/objectives/'+query['objectiveId'];
-
+      if( Config.settings.user_auth_token() )
+      {
+        url = url + '?proxyname=' + Config.settings.user_auth_token();
+      }
       resolve($.ajax({type: "DELETE", url: url, contentType: "application/json", dataType: 'json'}).then(function(response){
         return response;
       }));
